56adfbbf9c7a3d56bf8a391891ff47e8a4ede7c9
Dayem Siddiqui <dayem.siddiqui@personio.de>
2023-09-27 00:00:00 +0200

    Fixed the bug in booting postgres

---
+++ Features.md
@@ -0,0 +1,4 @@
+# Features to implement
+- [ ] Add option to load data from a pg_dump file
+- [ ] Add option to ensure that the database is empty before loading data
+- [ ] Add option to ensure that database version is compatible with the data from the dump file
--- docker/docker.go
+++ docker/docker.go
@@ -3,24 +3,28 @@ package docker
 import (
 	"log"
 	"os/exec"
-	"strings"
 )
 
-func IsContainerRunning() bool {
-	cmd := exec.Command("docker-compose", "ps", "-q", "postgres")
+func IsContainerNotRunning() bool {
+	log.Println("Checking if Postgres container is running...")
+	cmd := exec.Command("docker", "compose", "ps", "-q", "postgres")
 	output, err := cmd.Output()
 	if err != nil {
+		log.Fatalf("Failed to check if Postgres container is running: %s", err)
 		return false
 	}
-	return strings.TrimSpace(string(output)) != ""
+	return len(output) == 0
 }
 
 func RunContainer() {
-	if IsContainerRunning() {
-		cmd := exec.Command("docker-compose", "up", "-d")
+	if IsContainerNotRunning() {
+		log.Println("Starting Postgres container...")
+		cmd := exec.Command("docker", "compose", "up", "-d")
 		err := cmd.Run()
 		if err != nil {
 			log.Fatalf("Failed to start the Postgres container: %s", err)
 		}
+	} else {
+		log.Println("Postgres container is already running.")
 	}
 }
